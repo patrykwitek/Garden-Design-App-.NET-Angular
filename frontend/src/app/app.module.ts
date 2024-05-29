a16356a7a88ed1ee1e9fc3f33a715ae3ba7250ed
Patryk Witek <patryk.witolski@gmail.com>
2024-05-30 00:51:40 +0200

    PDA-001: navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ClickOutsideDirective } from './directives/clickOutside.directive';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { SharedModule } from './shared-modules/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ClickOutsideDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatIconModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/services/login.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  baseUrl = environment.apiUrl;
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'login/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    )
+  }
+
+  public register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'login/register', model).pipe(
+      map(user => {
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    );
+  }
+
+  public setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUserSource.next(user);
+  }
+
+  public logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+}
